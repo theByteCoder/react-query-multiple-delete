f0f5c79c25934eb46b5d9bc9e3d073bd8cd852bb
theByteCoder <47722399+theByteCoder@users.noreply.github.com>
2020-12-02 04:38:38 +0530

    feat(reactQuery): Added json server and modified package json and employees component. modified django views and urls

--- backend/urls.py
+++ backend/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 
 from backend.views import *
 
+# urlpatterns = [
+#     path('api/create/info=<str:info>/', create_record),
+#     path('api/employeesList/', get_record),
+#     path('api/getEmployees/', get_record_all),
+#     path('api/emp_no=<int:emp_no>/', get_record_single),
+#     path('api/update/info=<str:info>/', update_record),
+#     path('api/delete/emp_no=<int:emp_no>/', delete_record),
+# ]
+
 urlpatterns = [
-    path('api/create/info=<str:info>/', create_record),
-    path('api/employeesList/', get_record),
-    path('api/getEmployees/', get_record_all),
-    path('api/emp_no=<int:emp_no>/', get_record_single),
-    path('api/update/info=<str:info>/', update_record),
-    path('api/delete/emp_no=<int:emp_no>/', delete_record),
+    path('employees/<int:emp_no>', delete_record),
+    path('employees/', get_record_all),
 ]
